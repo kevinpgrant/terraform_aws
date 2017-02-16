7d542786193c9555ce4c428cbe826a2d04c16bab
Kevin Grant <kevinpgrant@yahoo.co.uk>
2017-02-16 08:44:58 +0000

    alter subnets to have a public and a private subnet in multiple AZs

--- aws_route_table_association.tf
+++ aws_route_table_association.tf
@@ -1,9 +1,19 @@
-resource "aws_route_table_association" "rta-1a" {
+resource "aws_route_table_association" "rta-1a-pub" {
     route_table_id = "${aws_route_table.rtb-main.id}"
-    subnet_id = "${aws_subnet.subnet-public.id}"
+    subnet_id = "${aws_subnet.subnet-1a-public.id}"
 }
 
-resource "aws_route_table_association" "rta-1b" {
+resource "aws_route_table_association" "rta-1a-priv" {
     route_table_id = "${aws_route_table.rtb-main.id}"
-    subnet_id = "${aws_subnet.subnet-private-with-nat.id}"
+    subnet_id = "${aws_subnet.subnet-1a-private-with-nat.id}"
+}
+
+resource "aws_route_table_association" "rta-1b-pub" {
+    route_table_id = "${aws_route_table.rtb-main.id}"
+    subnet_id = "${aws_subnet.subnet-1b-public.id}"
+}
+
+resource "aws_route_table_association" "rta-1b-priv" {
+    route_table_id = "${aws_route_table.rtb-main.id}"
+    subnet_id = "${aws_subnet.subnet-1b-private-with-nat.id}"
 }
--- aws_subnet.tf
+++ aws_subnet.tf
@@ -1,4 +1,4 @@
-resource "aws_subnet" "subnet-public" {
+resource "aws_subnet" "subnet-1a-public" {
     vpc_id                  = "${aws_vpc.main.id}"
     cidr_block              = "172.37.0.0/20"
     availability_zone       = "eu-west-1a"
@@ -10,7 +10,7 @@ resource "aws_subnet" "subnet-public" {
     }
 }
 
-resource "aws_subnet" "subnet-private-with-nat" {
+resource "aws_subnet" "subnet-1a-private-with-nat" {
     vpc_id                  = "${aws_vpc.main.id}"
     cidr_block              = "172.37.16.0/20"
     availability_zone       = "eu-west-1a"
@@ -22,3 +22,26 @@ resource "aws_subnet" "subnet-private-with-nat" {
     }
 }
 
+resource "aws_subnet" "subnet-1b-public" {
+    vpc_id                  = "${aws_vpc.main.id}"
+    cidr_block              = "172.37.128.0/20"
+    availability_zone       = "eu-west-1b"
+    map_public_ip_on_launch = true
+
+    tags {
+        Name = "w00t"
+        Terraform = "true"
+    }
+}
+
+resource "aws_subnet" "subnet-1b-private-with-nat" {
+    vpc_id                  = "${aws_vpc.main.id}"
+    cidr_block              = "172.37.144.0/20"
+    availability_zone       = "eu-west-1b"
+    map_public_ip_on_launch = true
+
+    tags {
+        Name = "w00t"
+        Terraform = "true"
+    }
+}
