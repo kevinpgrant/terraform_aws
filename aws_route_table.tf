af73939758c5bbdda957367be10d4cc4e9910857
Kevin Grant <kevinpgrant@yahoo.co.uk>
2017-02-16 08:32:18 +0000

    add route table

---
+++ aws_route_table.tf
@@ -0,0 +1,13 @@
+resource "aws_route_table" "rtb-main" {
+    vpc_id     = "${aws_vpc.main.id}"
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = "${aws_internet_gateway.igw-main.id}"
+    }
+
+    tags {
+        Name = "w00t"
+        Terraform = "true"
+    }
+}
