2b50498ab6f4d4596bced3b938594cd2d2a607c7
Kevin Grant <kevinpgrant@yahoo.co.uk>
2017-02-16 08:54:36 +0000

    no Elastic IPs for private subnet instances

--- aws_subnet.tf
+++ aws_subnet.tf
@@ -14,7 +14,7 @@ resource "aws_subnet" "subnet-1a-private-with-nat" {
     vpc_id                  = "${aws_vpc.main.id}"
     cidr_block              = "172.37.16.0/20"
     availability_zone       = "eu-west-1a"
-    map_public_ip_on_launch = true
+    map_public_ip_on_launch = false
 
     tags {
         Name = "w00t"
@@ -38,7 +38,7 @@ resource "aws_subnet" "subnet-1b-private-with-nat" {
     vpc_id                  = "${aws_vpc.main.id}"
     cidr_block              = "172.37.144.0/20"
     availability_zone       = "eu-west-1b"
-    map_public_ip_on_launch = true
+    map_public_ip_on_launch = false
 
     tags {
         Name = "w00t"
